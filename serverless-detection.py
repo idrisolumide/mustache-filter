c2f20c13b8acc289fb6ccc22d0b272b53bf806a0
ayushmaan6 <44741296+ayushmaan6@users.noreply.github.com>
2019-08-31 13:03:04 +0400

    Update serverless-detection.py

--- serverless-detection.py
+++ serverless-detection.py
@@ -1,84 +1,89 @@
 import cv2  # OpenCV Library  
 
-import http.client 
+import http.client
 import json
 import numpy as np
-import urllib.request 
-
+import urllib.request
 
 # get image from cloud functions then save it locally
 conn = http.client.HTTPSConnection("us-south.functions.cloud.ibm.com")
 
 headers = {
     'accept': "application/json",
-    'content-type': "application/json", 
-    }
+    'content-type': "application/json",
+}
 
-conn.request("POST", "add-your-https-url-here", headers=headers)
+conn.request("POST",
+             "https://us-south.functions.cloud.ibm.com/api/v1/web/Developer%20Advocacy_Cloud%20Developer%20Advocacy/default/dday2-ayush-mustache.json",
+             headers=headers)
 
 res = conn.getresponse()
 data = res.read()
 
 json_result = json.loads(data.decode("utf-8"))
 
-print(json_result) 
+print(json_result)
 
-picture = json_result['url'] 
+picture = json_result['url']
 
-print (picture)  
+print(picture)
 
-#even after saving it locally gives same result
+# even after saving it locally gives same result
 urllib.request.urlretrieve(picture, "local-filename.jpg")
 
-
- 
-#-----------------------------------------------------------------------------
+# -----------------------------------------------------------------------------
 #       Load and configure Haar Cascade Classifiers
-#-----------------------------------------------------------------------------
- 
+# -----------------------------------------------------------------------------
+# Object Detection using Haar feature-based cascade classifiers is an effective
+# object detection method proposed by Paul Viola and Michael Jones in their paper,
+# "Rapid Object Detection using a Boosted Cascade of Simple Features" in 2001.
+# It is a machine learning based approach where a cascade function is trained from a lot of positive and negative images.
+# It is then used to detect objects in other images.
+
+
 # location of OpenCV Haar Cascade Classifiers:
-#baseCascadePath = "/usr/local/share/OpenCV/haarcascades/"
- 
+# baseCascadePath = "/usr/local/share/OpenCV/haarcascades/"
+
 # xml files describing our haar cascade classifiers
 faceCascadeFilePath = "haarcascade_frontalface_default.xml"
 noseCascadeFilePath = "haarcascade_mcs_nose.xml"
- 
+
 # build our cv2 Cascade Classifiers
 faceCascade = cv2.CascadeClassifier(faceCascadeFilePath)
 noseCascade = cv2.CascadeClassifier(noseCascadeFilePath)
- 
-#-----------------------------------------------------------------------------
+
+# -----------------------------------------------------------------------------
 #       Load and configure mustache (.png with alpha transparency)
-#-----------------------------------------------------------------------------
- 
+# -----------------------------------------------------------------------------
+
 # Load our overlay image: mustache.png
-imgMustache = cv2.imread('local-filename.jpg',-1)
- 
+imgMustache = cv2.imread('local-filename.jpg', -1)
+
 # Create the mask for the mustache
-orig_mask = imgMustache[:,:,3]
- 
+orig_mask = imgMustache[:, :, 3]
+
 # Create the inverted mask for the mustache
 orig_mask_inv = cv2.bitwise_not(orig_mask)
- 
+
 # Convert mustache image to BGR
 # and save the original image size (used later when re-sizing the image)
-imgMustache = imgMustache[:,:,0:3]
+imgMustache = imgMustache[:, :, 0:3]
 origMustacheHeight, origMustacheWidth = imgMustache.shape[:2]
- 
-#-----------------------------------------------------------------------------
+
+# -----------------------------------------------------------------------------
 #       Main program loop
-#-----------------------------------------------------------------------------
- 
+# -----------------------------------------------------------------------------
+
 # collect video input from first webcam on system
 video_capture = cv2.VideoCapture(0)
- 
+
 while True:
     # Capture video feed
     ret, frame = video_capture.read()
- 
+
     # Create greyscale image from the video feed
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
- 
+
     # Detect faces in input video stream
     faces = faceCascade.detectMultiScale(
         gray,
@@ -87,32 +92,32 @@ while True:
         minSize=(30, 30),
         flags=cv2.CASCADE_SCALE_IMAGE
     )
- 
-   # Iterate over each face found
+
+    # Iterate over each face found
     for (x, y, w, h) in faces:
         # Un-comment the next line for debug (draw box around all faces)
         # face = cv2.rectangle(frame,(x,y),(x+w,y+h),(255,0,0),2)
- 
-        roi_gray = gray[y:y+h, x:x+w]
-        roi_color = frame[y:y+h, x:x+w]
- 
+
+        roi_gray = gray[y:y + h, x:x + w]
+        roi_color = frame[y:y + h, x:x + w]
+
         # Detect a nose within the region bounded by each face (the ROI)
         nose = noseCascade.detectMultiScale(roi_gray)
- 
-        for (nx,ny,nw,nh) in nose:
+
+        for (nx, ny, nw, nh) in nose:
             # Un-comment the next line for debug (draw box around the nose)
-            #cv2.rectangle(roi_color,(nx,ny),(nx+nw,ny+nh),(255,0,0),2)
- 
+            # cv2.rectangle(roi_color,(nx,ny),(nx+nw,ny+nh),(255,0,0),2)
+
             # The mustache should be three times the width of the nose
-            mustacheWidth =  3 * nw
+            mustacheWidth = 3 * nw
             mustacheHeight = mustacheWidth * origMustacheHeight / origMustacheWidth
- 
+
             # Center the mustache on the bottom of the nose
-            x1 = nx - (mustacheWidth/4)
-            x2 = nx + nw + (mustacheWidth/4)
-            y1 = ny + nh - (mustacheHeight/2)
-            y2 = ny + nh + (mustacheHeight/2)
- 
+            x1 = nx - (mustacheWidth / 4)
+            x2 = nx + nw + (mustacheWidth / 4)
+            y1 = ny + nh - (mustacheHeight / 2)
+            y2 = ny + nh + (mustacheHeight / 2)
+
             # Check for clipping
             if x1 < 0:
                 x1 = 0
@@ -122,43 +127,43 @@ while True:
                 x2 = w
             if y2 > h:
                 y2 = h
- 
+
             # Re-calculate the width and height of the mustache image
-            mustacheWidth = int (x2) - int (x1)
-            mustacheHeight = int (y2) - int (y1)
- 
+            mustacheWidth = int(x2) - int(x1)
+            mustacheHeight = int(y2) - int(y1)
+
             # Re-size the original image and the masks to the mustache sizes
             # calcualted above
-            mustache = cv2.resize(imgMustache, (mustacheWidth,mustacheHeight), interpolation = cv2.INTER_AREA)
-            mask = cv2.resize(orig_mask, (mustacheWidth,mustacheHeight), interpolation = cv2.INTER_AREA)
-            mask_inv = cv2.resize(orig_mask_inv, (mustacheWidth,mustacheHeight), interpolation = cv2.INTER_AREA)
- 
+            mustache = cv2.resize(imgMustache, (mustacheWidth, mustacheHeight), interpolation=cv2.INTER_AREA)
+            mask = cv2.resize(orig_mask, (mustacheWidth, mustacheHeight), interpolation=cv2.INTER_AREA)
+            mask_inv = cv2.resize(orig_mask_inv, (mustacheWidth, mustacheHeight), interpolation=cv2.INTER_AREA)
+
             # take ROI for mustache from background equal to size of mustache image
             roi = roi_color[int(y1):int(y2), int(x1):int(x2)]
- 
+
             # roi_bg contains the original image only where the mustache is not
             # in the region that is the size of the mustache.
-            roi_bg = cv2.bitwise_and(roi,roi,mask = mask_inv)
- 
+            roi_bg = cv2.bitwise_and(roi, roi, mask=mask_inv)
+
             # roi_fg contains the image of the mustache only where the mustache is
-            roi_fg = cv2.bitwise_and(mustache,mustache,mask = mask)
- 
+            roi_fg = cv2.bitwise_and(mustache, mustache, mask=mask)
+
             # join the roi_bg and roi_fg
-            dst = cv2.add(roi_bg,roi_fg)
- 
+            dst = cv2.add(roi_bg, roi_fg)
+
             # place the joined image, saved to dst back over the original image
             roi_color[int(y1):int(y2), int(x1):int(x2)] = dst
- 
+
             break
- 
+
     # Display the resulting frame
     cv2.imshow('Video', frame)
- 
+
     # press any key to exit
     # NOTE;  x86 systems may need to remove: " 0xFF == ord('q')"
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
- 
+
 # When everything is done, release the capture
 video_capture.release()
 cv2.destroyAllWindows()
